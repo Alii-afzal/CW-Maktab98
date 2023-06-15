e932a8aff272c619d8ff224105757f56e5ebbd20
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-06-15 15:31:26 +0330

    first commit

---
+++ 1.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+def read_file(file_name):
+    with open(file_name, "r") as file:
+        content = file.read()
+    return content
+
+if len(sys.argv) == 1:
+    print("Hello World!")
+elif os.path.exists(sys.argv[1]):
+    string = read_file(sys.argv[1], mod= "r")
+    lst1 = string.split()
+    dic1 = dict()
+    for word in lst1:
+        dic1[word] = dic1.get(word, 0) + 1
+    print(dic1)
+else:
+    print(sys.argv[1][::-1])
